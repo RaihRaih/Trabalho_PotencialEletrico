2d3a3748e6e5f71a95689381fed774966a62773e
Alberth Emanuel <emanuel.a.ildefonso@gmail.com>
2023-08-06 15:25:46 -0300

    Added

--- PotencialEletricoOficial.py
+++ PotencialEletricoOficial.py
@@ -1,5 +1,9 @@
 # Trabalho de Física - Potencial Elétrico
 # Alberth Emanuel - Carolina Fonseca - Raissa Maia - Igor Amorim
 
+#Extensões que serão usadas são: matplotlib, numpy e tkinter
+import tkinter as tk
+from tkinter import simpledialog
 
-print("teste")
+import matplotlib.pyplot as plt
+import numpy as np
