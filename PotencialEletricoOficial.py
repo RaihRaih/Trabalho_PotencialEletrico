b563da4349149392a61a527471c6b2310c8c6c1e
Alberth Emanuel <emanuel.a.ildefonso@gmail.com>
2023-08-06 15:42:39 -0300

    Added

--- PotencialEletricoOficial.py
+++ PotencialEletricoOficial.py
@@ -20,11 +20,35 @@ def selecionar_quantidade_cargas():
     
     return num_cargas
 
+#Função para selecionar os valores das cargas elétricas e suas posições
+def selecionar_valores_cargas(num_cargas):
+    
+    # Cria uma janela pop-up usando tkinter e pede ao usuário para inserir os valores das cargas e suas posições
+    cargas = []
+    root = tk.Tk()
+    root.withdraw()
+    
+    for i in range(num_cargas):
+        
+        carga = simpledialog.askfloat(f"Carga {i+1}", f"Digite a carga {i+1} (em Coulombs):")
+        pos_x = simpledialog.askfloat(f"Carga {i+1}", f"Digite a posição x da carga {i+1}:")
+        pos_y = simpledialog.askfloat(f"Carga {i+1}", f"Digite a posição y da carga {i+1}:")
+        cargas.append((carga, (pos_x, pos_y)))
+        
+    root.destroy()
+    return cargas
+
+
+
+
 
 def main():
     
     num_cargas = selecionar_quantidade_cargas()
     print(f"Quantidade de cargas selecionadas: {num_cargas}")
+    
+    valores_cargas = selecionar_valores_cargas(num_cargas)
+    print("Valores das cargas selecionados:", valores_cargas)
     
     
 if __name__ == "__main__":
