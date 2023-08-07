3f313b63f477b21c33c93ffa57cd78f30b10f3ff
carolfonseca14 <105356405+carolfonseca14@users.noreply.github.com>
2023-08-06 21:17:10 -0300

    Update PotencialEletricoOficial.py
    adicionei a função para desenhar as cargas no gráfico e criar os subplots para os gráficos

--- PotencialEletricoOficial.py
+++ PotencialEletricoOficial.py
@@ -90,6 +90,30 @@ def criar_grafico_contorno(cargas, resolucao = 100):
 
 # ************************************************************************************************
 
+# Função para desenhar as cargas no gráfico e criar os subplots para os gráficos
+def desenhar_grafico_cargas(cargas):
+    
+    global fig  
+    
+    # Criação da figura e dos subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+    # Desenho das cargas elétricas no primeiro subplot (gráfico 2D)
+    for carga, pos in cargas:
+        ax1.scatter(pos[0], pos[1], s=100, label=f"Carga: {carga}")
+
+    ax1.set_xlabel('Eixo x')
+    ax1.set_ylabel('Eixo y')
+    ax1.legend()
+
+    # Modo interativo
+    plt.ion()
+    plt.show()
+
+    return ax1, ax2
+
+# ************************************************************************************************
+
 ## Criar gráfico das cargas.
 
 def main():
